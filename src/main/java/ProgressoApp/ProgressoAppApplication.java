2797715cc192ffea7c26888ea95838c266c5b508
lewyczlowiek <tomek.leewandowski@gmail.com>
2025-03-12 15:47:07 +0100

    initial commit

---
+++ src/main/java/ProgressoApp/ProgressoAppApplication.java
@@ -0,0 +1,13 @@
+package ProgressoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProgressoApp/ProgressoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ProgressoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
