e3a8b425ed3a653039f0899dc675af364f87451f
Krzysztof <thekriso@wp.pl>
2025-05-22 22:56:58 +0200

    Added new project tests

--- src/main/java/ProgressoApp/service/ProjectService.java
+++ src/main/java/ProgressoApp/service/ProjectService.java
@@ -8,7 +8,11 @@ import java.util.Optional;
 
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @Transactional
@@ -29,5 +33,11 @@ public class ProjectService {
   public List<Project> getAllProjects() {
     return projectRepository.findAll();
   }
+
+  public Project getProjectById(long id) {
+    return projectRepository.findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Project not found"));
+  }
+
 
 }
--- src/test/java/ProgressoApp/project/ProjectServiceTest.java
+++ src/test/java/ProgressoApp/project/ProjectServiceTest.java
@@ -3,12 +3,18 @@ package ProgressoApp.project;
 import ProgressoApp.dto.request.ProjectRequestDTO;
 import ProgressoApp.model.Project;
 import ProgressoApp.service.ProjectService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
@@ -16,6 +22,9 @@ import java.util.HashSet;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.StatusResultMatchersExtensionsKt.isEqualTo;
 
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
@@ -25,20 +34,58 @@ public class ProjectServiceTest {
     @Autowired
     private ProjectService projectService;
 
-    @Test
-    void shouldSaveAndFindProject() {
-        ProjectRequestDTO dto = new ProjectRequestDTO(
+
+    private ProjectRequestDTO dto;
+    private Project savedProject;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        dto = new ProjectRequestDTO(
                 "Zadanie 1",
                 "Napisać test",
-                new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime(),
+                LocalDateTime.of(2024, 5, 1, 12, 0),
                 new ArrayList<>(),
                 new HashSet<>()
         );
 
         projectService.createProject(dto);
-        List<Project> list = projectService.getAllProjects();
 
-        assertThat(list).hasSize(1);
-        assertThat(list.get(0).getDescription()).isEqualTo("Napisać test");
+        savedProject = new Project(dto);
+        savedProject.setProjectId(1L);
+
+
     }
+
+    @Test
+    void shouldSaveProject() {
+
+        projectService.createProject(dto);
+        List<Project> projects = projectService.getAllProjects();
+
+        assertThat(projects).hasSize(4);
+        assertThat(projects.get(0).getName()).isEqualTo("Zadanie 1");
+        assertThat(projects.get(0).getDescription()).isEqualTo("Napisać test");
+    }
+
+    @Test
+    void shouldFindProjectById() {
+
+        Project response = projectService.getProjectById(1L);
+
+        assertThat(response.getName()).isEqualTo("Zadanie 1");
+        assertThat(response.getDescription()).isEqualTo("Napisać test");
+    }
+
+    @Test
+    void shouldThrowNotFoundWhenProjectMissing() {
+
+        assertThatThrownBy(() -> projectService.getProjectById(999L)).isInstanceOf(
+                        ResponseStatusException.class)
+                .hasMessageContaining("404 NOT_FOUND")
+                .hasMessageContaining("Project not found");
+    }
+
+
 }
