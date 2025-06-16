673e01b25d83c91fcc105d7eb977907e942f6189
lewyczlowiek <tomek.leewandowski@gmail.com>
2025-06-16 20:55:33 +0200

    Obsługa User - serwis, kontroler, repozytorium, paginacja

--- src/main/java/ProgressoApp/dto/response/UserResponseDTO.java
+++ src/main/java/ProgressoApp/dto/response/UserResponseDTO.java
@@ -4,8 +4,13 @@ import ProgressoApp.model.Role;
 import ProgressoApp.model.TaskSubmission;
 import java.util.List;
 
-public record UserResponseDTO(Long userId, String firstName, String lastName, String email,
-                              Role role, String numberIndex,
-                              List<TaskSubmissionResponseDTO> submissions) {
+public record UserResponseDTO(
 
-}
+        Long userId,
+        String firstName,
+        String lastName,
+        String email,
+        Role role,
+        String numberIndex,
+        List<TaskSubmissionResponseDTO> submissions
+){}
