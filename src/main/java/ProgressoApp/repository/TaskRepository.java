63a920570ae4f67934ce33b97ea1a2613f2e8305
BaKirooll <98025353+BaKirooll@users.noreply.github.com>
2025-06-25 01:19:52 +0200

    poprawione usuwanie

--- src/main/java/ProgressoApp/repository/TaskRepository.java
+++ src/main/java/ProgressoApp/repository/TaskRepository.java
@@ -1,11 +1,13 @@
 package ProgressoApp.repository;
 
 import ProgressoApp.model.Task;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -17,5 +19,10 @@ public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificat
 
        @Query("SELECT t FROM Task t JOIN t.users u WHERE u.userId = :userId")
        Page<Task> findTasksAssignedDirectly(@Param("userId") Long userId, Pageable pageable);
+
+       @Modifying
+       @Transactional
+       @Query(value = "DELETE FROM task_user WHERE user_id = :userId", nativeQuery = true)
+       void deleteAllTaskUserByUserId(@Param("userId") Long userId);
 
 }
